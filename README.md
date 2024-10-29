f7d7aced75397aa0a4fb658673dc179193b7ef4f
Freddy Bijanja <freddybijanja31@gmail.com>
2024-10-29 13:06:20 +0200

    add learning app

---
+++ README.md
@@ -0,0 +1,3 @@
+# learning App
+
+This is an app intended to link students with courses published by different teachers.
---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+const App = () => {
+  return ( 
+    <div className="text-xl text-center uppercase underline">Learning app </div>
+   );
+}
+ 
+export default App;
+
